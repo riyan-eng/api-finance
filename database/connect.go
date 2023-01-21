c333db761c429aaddbb98aa74e5f5873f56b854b
riyan-eng <fbriyant11@gmail.com>
2023-01-21 21:08:55 +0700

    migration table

--- database/connect.go
+++ database/connect.go
@@ -1,1 +1,46 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+var DB *gorm.DB
+
+func ConnectDb() {
+	var err error
+	var sqlDB *sql.DB
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=%v", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("DB_TIMEZONE"))
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "finance.",
+			SingularTable: false,
+		},
+	})
+
+	if err != nil {
+		panic("Connection failed to Database")
+	}
+
+	// connection pool
+	sqlDB, err = DB.DB()
+	sqlDB.SetConnMaxIdleTime(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	if err != nil {
+		panic("Connection pool failed")
+	}
+
+	fmt.Println("Connection Opened to Database")
+
+	// migrate database
+	DB.AutoMigrate(&COA{}, &GeneralLedger{})
+	fmt.Println("Database migrated")
+}
--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/riyan-eng/api-finance/config"
+	"github.com/riyan-eng/api-finance/database"
+)
+
+func init() {
+	config.LoadEnv()
+	database.ConnectDb()
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Use(recover.New())
+	app.Use(cors.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		err := c.SendString("help me!!!")
+		return err
+	})
+
+	app.Listen(":3000")
+}

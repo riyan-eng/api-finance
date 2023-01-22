cc055e7d312d79949a01d96a9fba2035d7d0d468
riyan-eng <fbriyant11@gmail.com>
2023-01-22 20:17:50 +0700

    controller cash receipt

--- src/finance/controllers/cash_controller.go
+++ src/finance/controllers/cash_controller.go
@@ -4,14 +4,25 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/riyan-eng/api-finance/src/finance/controllers/dto"
 	"github.com/riyan-eng/api-finance/src/finance/controllers/validator"
+	"github.com/riyan-eng/api-finance/src/finance/services"
 )
 
-type CashControllers interface {
+type CashController interface {
 	CashReceipt(c *fiber.Ctx) error
-	CashPayment(c *fiber.Ctx) error
+	// CashPayment(c *fiber.Ctx) error
 }
 
-func CashReceipt(c *fiber.Ctx) error {
+type cashController struct {
+	CashService services.CashService
+}
+
+func NewCashController(cashService services.CashService) CashController {
+	return &cashController{
+		CashService: cashService,
+	}
+}
+
+func (service cashController) CashReceipt(c *fiber.Ctx) error {
 	cashReceiptBody := new(dto.CashReceiptReq)
 
 	// parsing body json
@@ -26,6 +37,15 @@ func CashReceipt(c *fiber.Ctx) error {
 	if err := validator.CashReceipt(*cashReceiptBody); err != nil {
 		c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"data":    err,
+			"message": "fail",
+		})
+	}
+
+	// comunicate with service
+	err := service.CashService.CashReceipt(*cashReceiptBody)
+	if err != nil {
+		c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"data":    err.Error(),
 			"message": "fail",
 		})
 	}
--- src/finance/routes/cash_route.go
+++ src/finance/routes/cash_route.go
@@ -2,18 +2,25 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/riyan-eng/api-finance/database"
 	"github.com/riyan-eng/api-finance/src/finance/controllers"
+	"github.com/riyan-eng/api-finance/src/finance/repositories"
+	"github.com/riyan-eng/api-finance/src/finance/services"
 )
 
 type RouterStruct struct {
 	fiber.Router
 }
 
+func NewCashRoute(route RouterStruct) RouterStruct {
+	return route
+}
+
 func (r *RouterStruct) CashRoutes(router fiber.Router) {
-	// cashRepository:=""
-	// cashService:=""
-	// cashController=""
+	cashRepository := repositories.NewCashRepository(database.DB)
+	cashService := services.NewCashService(cashRepository)
+	cashController := controllers.NewCashController(cashService)
 
 	cash := r.Group("/cash")
-	cash.Post("/receipt", controllers.CashReceipt)
+	cash.Post("/receipt", cashController.CashReceipt)
 }

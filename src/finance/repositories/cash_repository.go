352605d5b1ca88bf3eb54861477afe0e31cac968
riyan-eng <fbriyant11@gmail.com>
2023-01-24 07:46:36 +0700

    repository sales

--- src/finance/repositories/cash_repository.go
+++ src/finance/repositories/cash_repository.go
@@ -147,6 +147,64 @@ func (cash *cashRepository) CashPayment(cashEntity entities.CashReceipt) error {
 	return tx.Commit().Error
 }
 
-func (cash *cashRepository) Sales(entity entities.CashReceipt) error {
-	return nil
+func (cash *cashRepository) Sales(cashEntity entities.CashReceipt) error {
+	// debet struct
+	var cashModelDebet models.CashModel = models.CashModel{
+		ID:     uuid.NewString(),
+		UserID: cashEntity.UserID,
+		Code:   cashEntity.Journal.Debet.Code,
+		Name:   cashEntity.Journal.Debet.Name,
+		Amount: cashEntity.Journal.Debet.Amount,
+	}
+	// credit struct
+	var cashModelCredit models.CashModel = models.CashModel{
+		ID:     uuid.NewString(),
+		UserID: cashEntity.UserID,
+		Code:   cashEntity.Journal.Credit.Code,
+		Name:   cashEntity.Journal.Credit.Name,
+		Amount: cashEntity.Journal.Credit.Amount,
+	}
+
+	// transaction query
+	queryTransaction := fmt.Sprintf(`
+		INSERT INTO finance.transactions (id, date_time, description, amount, user_id) VALUES ('%s', current_timestamp, '%s', '%f', '%s')
+	`, cashEntity.ID, cashEntity.Description, cashEntity.Amount, cashEntity.UserID)
+	queryDebet := fmt.Sprintf(`
+		INSERT INTO finance.general_ledgers (id, transaction, coa, debet, user_id) VALUES ('%s', '%s', '%s', '%f', '%s')
+	`, cashModelDebet.ID, cashEntity.ID, cashModelDebet.Code, cashModelDebet.Amount, cashModelDebet.UserID)
+	// credit query
+	queryCredit := fmt.Sprintf(`
+		INSERT INTO finance.general_ledgers (id, transaction, coa, credit, user_id) VALUES ('%s', '%s', '%s', '%f', '%s')
+	`, cashModelCredit.ID, cashEntity.ID, cashModelCredit.Code, cashModelCredit.Amount, cashModelCredit.UserID)
+
+	tx := cash.DB.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		return err
+	}
+
+	// insert transaction
+	if err := tx.Exec(queryTransaction).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// insert debet
+	if err := tx.Exec(queryDebet).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// insert credit
+	if err := tx.Exec(queryCredit).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
 }
